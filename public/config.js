73fe3b5e5ff1051b6cc5493950d73644d4265b00
orderly-guoqing <guoqing@orderly.network>
2026-06-14 23:17:47 +0800

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -18,7 +18,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_APP_DESCRIPTION": "Reinventing the Decentralized Exchange Revenue Model.",
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
-  "VITE_ENABLED_MENUS": "Trading,Markets,Swap,Leaderboard,Points,Portfolio,Campaigns,Vaults",
+  "VITE_ENABLED_MENUS": "Trading,Markets,Swap,Leaderboard,Points,Portfolio,Vaults",
   "VITE_CUSTOM_MENUS": "",
   "VITE_ENABLE_SERVICE_DISCLAIMER_DIALOG": "true",
   "VITE_ENABLE_CAMPAIGNS": "false",
